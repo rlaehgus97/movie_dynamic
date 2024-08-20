d1ddf1d0075e4797796c143bdfebe8fa59dc5cdb
rlaehgus97 <starkim625@gmail.com>
2024-08-20 13:53:17 +0900

    dag edit

--- dags/movies-dynamic-json.py
+++ dags/movies-dynamic-json.py
@@ -10,60 +10,57 @@ from airflow.operators.python import (
     ExternalPythonOperator,
     PythonOperator,
     PythonVirtualenvOperator,
-    is_venv_installed,
     BranchPythonOperator
 )
-import os
+
+REQUIREMENTS = "git+HTTPS://github.com/rlaehgus97/mdata0814.git"
 
 with DAG(
-    'parsing',
+    'movie_dynamic',
     default_args={
         'depends_on_past': False,
-        'retries': 1,
+        'retries': 0,
         'retry_delay': timedelta(seconds=3)
     },
-    max_active_runs=1,
-    max_active_tasks=3,
-    description='movie_extract',
-    schedule="10 2 * * *",
+    #max_active_runs=1,
+    #max_active_tasks=3,
+    description='movie dynamic dag',
+    schedule="@once",
     start_date=datetime(2015, 1, 1),
-    end_date=datetime(2016, 1, 1),
+    end_date=datetime(2015, 1, 2),
     catchup=True,
-    tags=['movie', 'extract'],
+    tags=['movie', 'pyspark'],
 ) as dag:
 
-
-    def tmp():
-        pass
+    def getdata(ds_nodash):
+        year=ds_nodash[0:4]
+        print(year)
+        from movdata.ml import save_movies
+        save_movies(year)
 
     get_data = PythonVirtualenvOperator(
-        task_id="get.data",
-        python_callable=tmp,
-        requirements=["git+https://github.com/rlaehgus97/movie_dynamic.git@d0.2.0/movie_flow"],
-        system_site_packages=False,
+        task_id = "get.data",
+        python_callable = getdata,
+        requirements = REQUIREMENTS,
+        system_site_packages = False,
     )
 
-    parsing_parquet = PythonVirtualenvOperator(
+    parsing_parquet = BashOperator(
         task_id="parsing.parquet",
-        python_callable=tmp,
-        requirements=["git+https://github.com/rlaehgus97/movie_dynamic.git@d0.2.0/movie_flow"],
-        system_site_packages=False,
+        bash_command="""
+            YEAR={{ ds_nodash[:4] }}
+            $SPARK_HOME/bin/spark-submit /home/dohyun/codes/spark_flow/py/flat.py $YEAR
+        """
     )
 
-    select_parquet = PythonVirtualenvOperator(
+    select_parquet = BashOperator(
         task_id="select.parquet",
-        python_callable=tmp,
-        requirements=["git+https://github.com/rlaehgus97/movie_dynamic.git@d0.2.0/movie_flow"],
-        system_site_packages=False,
+        bash_command="""
+            $SPARK_HOME/bin/spark-submit /home/centa/code/spark_flow/py/my_select.py 
+        """
     )
 
-    start = EmptyOperator(
-        task_id="start",
-                )
-
-    end = EmptyOperator(
-        task_id="end",
-                )
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
     
-
     start >> get_data >> parsing_parquet >> select_parquet >> end 
